fa561d94f9d5e481d06e58694cad33cb2b1743a2
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-06-26 18:23:23 -0500

    jercicios de práctica para memorizar sintaxis en Visual studio code

---
+++ Practica(VS Code)/mostrarInfo.js
@@ -0,0 +1,9 @@
+//Ejemplo 1 Crear un programa que muestre en la consola: Hola mundo
+console.log("Hola mundo");
+//Ejemplo 2  Crear un programa que muestre en la consola tu nombre.
+let nombre = "Sneyder";
+let apellido = "Rodriguez";
+console.log("Hola, tu nombre y apellido son: " + apellido + "  " + nombre);
+//Ejemplo 3  Crear un programa que muestre en la consola Nombre y Edad
+let edad = 31;
+console.log("Hola, tu nombre y apellido son: " + apellido + "  " + nombre + " " + "Y tienes " + edad + " "+ "años" );
---
+++ Practica(VS Code)/operaciones.js
@@ -0,0 +1,11 @@
+// Crear dos variables y Mostrar: suma, resta, multiplicación, división.
+let num1 = 7;
+let num2 = 15;
+let suma = num1 + num2;
+let multiplicacion = num2 * num1;
+let resta = num2 - num1;
+let division = num2 / num1;
+console.log("El resultado de la suma es: " + suma);
+console.log("El resultado de la resta es: " + resta);
+console.log("El resultado de la multiplicación es: " + multiplicacion);
+console.log("El resultado de la división es: " + division);
